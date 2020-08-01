a4e81bacb65e5ee3cc075fc7b174c47b49e9a1e4
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-08-01 18:58:39 +0530

    added more fields to the user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,20 +12,34 @@ const UserSchema = new mongoose.Schema({
         match: [/.+\@.+\..+/, 'Please fill a valid email address'],
         required: 'Email is required'
     },
-    // remaining fields:
     // password
     password: {
         type: String,
         trim: true,
+        required: true
         //add validation for password
-    }
+    },
     // imagePath
+    imagePath: {
+        type: String,
+        trim: true,
+        default: ''
+    },
     // bio
+    bio: {
+        type: String,
+        trim: true,
+        default: ''
+    },
     // followers list
+    followers: [
+        { type: mongoose.Schema.ObjectId, ref: "User" }
+    ],
     // following list
-
+    following: [
+        { type: mongoose.Schema.ObjectId, ref: "User" }
+    ]
 });
 module.exports = {
     UserSchema: mongoose.model('User', UserSchema)
 }
-// export default mongoose.model('User', UserSchema);
